e4e234598d1ff3c8f3b08acab68025e30f47b4ac
Asaad Hussain <asaadhussain2014@gmail.com>
2024-10-30 14:50:28 +0500

    setup

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 import express from "express";
 import dotenv from "dotenv";
 
+
+dotenv.config();
 const port = process.env.PORT;
-const app = express();
+const app = express();                //create instance 
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);           //connect to server
+});
 
