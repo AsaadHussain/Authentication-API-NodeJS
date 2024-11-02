011dc302c3f904ca8c83de8e243a2313611cc587
Asaad Hussain <asaadhussain2014@gmail.com>
2024-11-02 12:48:54 +0500

    jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 
 import userModel from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 class UserController {
 
@@ -9,7 +10,6 @@ class UserController {
 
         const { name, email, password, password_confirm, tc } = req.body;
         const user = await userModel.findOne({ email: email });
-        console.log(tc);
 
         if (user) {
             res.send({ "status": "Failed", "message": "Email Already exists" });
@@ -29,8 +29,14 @@ class UserController {
                                 password: hashedPassword,
                                 tc: tc
                             });
+
                             await newUser.save();
-                            res.send({ "status": "Success", "message": "User Registered" });
+
+                            const savedUser = await userModel.findOne({ email });
+
+                            const token = jwt.sign({ userID: savedUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
+
+                            res.status(201).send({ "status": "Success", "message": "User Registered", "token": token });
                         } catch (error) {
                             res.send({ "status": "Failed", "message": error.message });
                         }
@@ -47,6 +53,41 @@ class UserController {
             } else {
                 res.send({ "status": "Failed", "message": "All fields are required" });
             }
+        }
+    };
+
+    static userLogin = async (req, res) => {
+        try {
+
+            const { email, password } = req.body;
+
+            if (email && password) {
+
+                const user = await userModel.findOne({ email });
+
+                if (user) {
+
+                    const isMatch = await bcrypt.compare(password, user.password);
+
+                    if ((email === user.email) && isMatch) {
+                        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' })
+                        res.send({ "status": "Success", "message": "You are Logged In", "token": token });
+
+                    } else {
+                        res.send({ "status": "Failed", "message": "Invalid email or password" });
+                    }
+
+                } else {
+                    res.send({ "status": "Failed", "message": "You are not registered" });
+                }
+
+            } else {
+                res.send({ "status": "Failed", "message": "All fields are required" });
+            }
+
+
+        } catch (error) {
+            res.send({ "status": "Failed", "message": error.message });
         }
     };
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();              //creating instance
 
 //public route
 router.post('/register', UserController.userRegistration);                    
+router.post('/login', UserController.userLogin);                    
 
 //protected route
 
